0097872d69a15acc7cadcdde8dab2e3a37dfccde
tivi <tivialvarez@gmail.com>
2025-11-16 08:23:49 -0500

    Add searchbar component to main app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component} from '@angular/core';
 import { WeatherData } from "./components/weather-data/weather-data";
+import { Searchbar } from "./components/searchbar/searchbar";
+
+
+type Coords = {
+  lat: string,
+  lon: string
+}
 
 @Component({
   selector: 'app-root',
-  imports: [WeatherData],
+  imports: [WeatherData, Searchbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-
 export class App {
+  selectedCity : Coords | null = null;
 }
