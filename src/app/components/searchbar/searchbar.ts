e436112e175cb7bc7573274d0e019f118c234add
tivi <tivialvarez@gmail.com>
2025-11-16 08:23:24 -0500

    Create searchbar component for finding location based on user input

---
+++ src/app/components/searchbar/searchbar.ts
@@ -0,0 +1,57 @@
+import { Component, inject, signal } from '@angular/core';
+import { GeocodingService } from '../../services/geocoding-service';
+import { REQ_STATUS } from '../../types/request-status';
+
+
+@Component({
+  selector: 'app-searchbar',
+  imports: [],
+  templateUrl: './searchbar.html',
+  styleUrl: './searchbar.scss',
+  providers: [GeocodingService]
+})
+export class Searchbar {
+  readonly REQ_STATUS = REQ_STATUS
+  geocoding = inject(GeocodingService)
+  location = signal("");
+  suggestions = signal<Array<any>>([]);
+  showSuggestions = signal<boolean>(false);
+  suggestionsStatus = signal<REQ_STATUS>(REQ_STATUS.REQ_NOT_STARTED)
+  searchTimeout = 0
+
+  handleLocationFocus(e: FocusEvent) {
+    this.showSuggestions.set(true)
+  }
+  
+  handleLocationBlur(e: FocusEvent) {
+    this.showSuggestions.set(false)
+  }
+
+  handleLocationSearch(e: KeyboardEvent) {
+    let inputEl = <HTMLInputElement> e.currentTarget;
+    this.location.set(inputEl.value);
+    clearTimeout(this.searchTimeout);
+    if(this.location().length > 0) {
+      //slight delay so as to not spam requests for every key stroke
+      this.searchTimeout = setTimeout(this.requestLocationSuggestions.bind(this), 500);
+    } else {
+      this.suggestions.set([]);
+      this.suggestionsStatus.set(REQ_STATUS.REQ_NOT_STARTED)
+    }
+  }
+
+  requestLocationSuggestions() {
+    this.suggestionsStatus.set(REQ_STATUS.REQ_PENDING)
+    this.geocoding.getLocationSuggestions(this.location()).subscribe({
+      next: (response : any) => {
+        this.suggestionsStatus.set(REQ_STATUS.REQ_SUCCESS)
+        console.log(response);
+        this.suggestions.set(response);
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.suggestionsStatus.set(REQ_STATUS.REQ_ERR)
+      }      
+    })
+  }
+}
